a2c16981696fdfdd9adf10148abcf3ba46b3b213
Hawasandra1 <Hawasandra.m@gmail.com>
2025-02-25 14:02:46 +0300

    Delete src directory

--- src/utils/dbConfig.jsx
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-const sql = neon(
-  "postgresql://neondb_owner:npg_HqKEf2Sw0lid@ep-round-lab-a44isv7k-pooler.us-east-1.aws.neon.tech/FinTable?sslmode=require"
-);
-export const db = drizzle(sql, { schema });
